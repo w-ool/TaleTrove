b120f6e46f65d6b5b925bcc9608c12684a9077ed
dongmyung <129810810+dongmyung0322@users.noreply.github.com>
2023-08-28 16:44:54 +0900

    08/28

---
+++ combine_crawling.py
@@ -0,0 +1,26 @@
+import os
+import pandas as pd
+
+input = './crawling_data/'
+
+output = './crawling_data/webtoon_data.csv'
+
+csv_files = [f for f in os.listdir(input) if f.endswith('.csv')]
+
+# csv파일들 읽어와서 하나의 리스트에 저장
+data_frames = []
+for csv_file in csv_files:
+    csv_path = os.path.join(input, csv_file)
+    df = pd.read_csv(csv_path)
+    data_frames.append(df)
+
+# dataframe 합치기
+merged_df = pd.concat(data_frames, ignore_index=True)
+# id열 삭제
+merged_df = merged_df.drop('id', axis=1)
+# 중복 삭제
+merged_df = merged_df.drop_duplicates(subset=['title'], keep='first')
+
+merged_df.to_csv(output, index=False, encoding='utf-8-sig')
+
+print("Finished")
